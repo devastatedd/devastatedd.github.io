aa1bf99627d34caec5c7313b5a1be336aede288f
Jetbo <jetbo@users.noreply.github.com>
2018-09-10 22:56:49 -0700

    add loading icon

--- js/menu.js
+++ js/menu.js
@@ -2,14 +2,12 @@ $(document).ready(function() {
   $('#menu-hamburger').on('click', function(event) {
     $icon = $(event.currentTarget);
     if($icon.hasClass('menu-closed')) {
-      console.log('open');
       $('#menu-links').show();
       $('#content').hide();
       $('#logo').hide();
       $('#menu').addClass('mobile-menu');
       $icon.removeClass('menu-closed');
     } else {
-      console.log('close');
       $('#menu-links').hide();
       $('#content').show();
       $('#logo').show();
